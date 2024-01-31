c15487e1f802114c0e129803f3eaaad8c25d3e10
Devam Mondal <devammondal06@gmail.com>
2024-01-30 23:13:01 -0500

    reversing a number

---
+++ src/ReversingNumber.java
@@ -0,0 +1,22 @@
+public class ReversingNumber {
+
+    public static void main (String[] args) {
+
+        int number = 123;
+        int reverse = 0;
+
+        while (number > 0) {
+
+            int lastDigit = number % 10;
+            reverse *= 10;
+            reverse += lastDigit;
+            number = number/10;
+
+        }
+
+        System.out.println("Reverse: " + reverse);
+
+
+    }
+
+}
