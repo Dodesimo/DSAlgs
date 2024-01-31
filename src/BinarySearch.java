5a60b9e6c7e6fc21b68c3edc36aa0e69d3369279
Devam Mondal <devammondal06@gmail.com>
2024-01-30 20:17:36 -0500

    Initial commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,46 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+     int[] orderedArray = new int[5];
+     orderedArray[0] = 1;
+     orderedArray[1] = 2;
+     orderedArray[2] = 3;
+     orderedArray[3] = 4;
+     orderedArray[4] = 5;
+
+
+     int lower = 0;
+     int higher = orderedArray.length-1;
+     int target = 4;
+
+     while (true) {
+
+         int current = (lower + higher)/2;
+
+         if (orderedArray[current] == target) {
+
+             System.out.println(current);
+             break;
+
+         } else if (target > orderedArray[current]) {
+
+             lower = current + 1;
+
+         } else if (target < orderedArray[current]) {
+
+             higher = current - 1;
+
+         } else if (lower > higher) {
+
+             System.out.println("Not found");
+
+         }
+
+
+
+     }
+
+
+    }
+}
