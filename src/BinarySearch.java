0c7b560861de380263b213a2717d8b2c6b9fd8fb
Devam Mondal <devammondal06@gmail.com>
2024-01-30 20:21:00 -0500

    exception cases first

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -12,7 +12,7 @@ public class BinarySearch {
 
      int lower = 0;
      int higher = orderedArray.length-1;
-     int target = 4;
+     int target = 7;
 
      while (true) {
 
@@ -23,24 +23,23 @@ public class BinarySearch {
              System.out.println(current);
              break;
 
-         } else if (target > orderedArray[current]) {
-
-             lower = current + 1;
-
-         } else if (target < orderedArray[current]) {
-
-             higher = current - 1;
-
          } else if (lower > higher) {
 
-             System.out.println("Not found");
+             System.out.println(orderedArray.length);
+             break;
 
+         }  else {
+
+             if (target > orderedArray[current]) {
+
+                 lower = current + 1;
+
+             } else if (target < orderedArray[current]) {
+
+                 higher = current - 1;
+
+             }
          }
-
-
-
      }
-
-
     }
 }
