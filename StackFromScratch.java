9f4e4a6179c3d60ffd7d006a56eb967d9a046a18
Devam Mondal <devammondal06@gmail.com>
2024-02-11 19:44:09 -0500

    stack from scratch

---
+++ StackFromScratch.java
@@ -0,0 +1,51 @@
+public class StackFromScratch {
+
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public StackFromScratch(int s) {
+
+        maxSize = s;
+        stackArray = new Array[s];
+        top = 0;
+
+    }
+
+    public void push(int j) {
+
+        stackArray[top] = j;
+        top++;
+
+    }
+
+    public int pop() {
+
+        target = stackArray[top];
+        stackArray[top] = 0;
+        top--;
+
+        return target;
+
+
+    }
+
+    public int peek() {
+
+        return stackArray[top];
+
+    }
+
+    public boolean empty() {
+
+        return top == 0;
+
+    }
+
+    public boolean isFull() {
+
+        return top == maxSize;
+
+    }
+
+}
