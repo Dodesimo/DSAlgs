8569e2744e8348c87279a96c40948344c7967598
Devam Mondal <devammondal06@gmail.com>
2024-02-17 12:49:14 -0500

    link

---
+++ src/Link.java
@@ -0,0 +1,21 @@
+public class Link {
+
+    public int iData;
+    public double dData;
+    public Link nextItem;
+
+    public Link(int i, double d) {
+
+        iData = i;
+        dData = d;
+        //nextItem is automatically set to a null value.
+
+    }
+
+    public void displayLink() {
+
+        System.out.println("{" + iData + ", " + dData + "}" );
+
+    }
+
+}
