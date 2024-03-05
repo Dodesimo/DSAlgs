19a43f936d5c6e65d83544625a63954c161415ef
Devam Mondal <devammondal06@gmail.com>
2024-03-04 22:52:28 -0500

    stack from linked list

---
+++ src/StackFromLinkedList.java
@@ -0,0 +1,35 @@
+public class StackFromLinkedList {
+
+    public SimpleSingleLinkedList sl;
+
+    public StackFromLinkedList() {
+
+        sl = new SimpleSingleLinkedList();
+
+    }
+
+    public void push(double j){
+
+        sl.insertFirst(0, j);
+
+    }
+    public double pop () {
+
+        Link link = sl.delete(0);
+        return link.dData;
+
+    }
+    public boolean isEmpty(){
+
+        return sl.isEmpty();
+
+    }
+    public void displayStack() {
+
+        sl.displayList();
+
+    }
+
+
+
+}
