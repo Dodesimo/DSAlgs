32365954326a352cfbe575f40f4d33602a66fe0a
Devam Mondal <devammondal06@gmail.com>
2024-04-09 23:28:32 -0400

    doubly link

---
+++ src/DoublyLink.java
@@ -0,0 +1,19 @@
+public class DoublyLink {
+
+    public long dData;
+    public DoublyLink previous;
+    public DoublyLink next;
+
+    public DoublyLink(long d) {
+
+        dData = d;
+
+    }
+
+    public void displayLink() {
+
+        System.out.println(dData + " ");
+
+    }
+
+}
