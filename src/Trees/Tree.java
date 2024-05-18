4b3690113e67395514f44c7fc9a823b2b6c11999
Devam Mondal <devammondal06@gmail.com>
2024-05-17 23:02:03 -0400

    instantiate tree

--- src/Trees/Tree.java
+++ src/Trees/Tree.java
@@ -1,2 +1,15 @@
-package Trees;public class Tree {
+package Trees;
+
+public class Tree {
+
+    private Node root;
+
+    public void find(int key){}
+
+    public void insert(int id, double dd){}
+
+    public void delete (int id){}
+
+
+
 }
