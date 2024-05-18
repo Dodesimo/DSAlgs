cea816b922c9ce1459cfaf8cb094c6fba36774dc
Devam Mondal <devammondal06@gmail.com>
2024-05-17 22:59:14 -0400

    basic node

---
+++ src/Trees/Node.java
@@ -0,0 +1,17 @@
+package Trees;
+
+public class Node {
+
+    int iData;
+    double dData;
+    Node lNode;
+    Node rNode;
+
+    public void displayNode(){
+
+        System.out.println(iData + dData);
+
+    }
+
+
+}
